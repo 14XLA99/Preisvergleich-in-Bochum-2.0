6e634fd7d84426c6b9f90e3e670869720d50959f
14XLA99 <lars.aretz0@gmail.com>
2025-05-26 20:11:46 +0200

    Update and rename firebaseAdmin.ts to firebaseAdmin.js

--- pages/api/firebaseAdmin.js
+++ pages/api/firebaseAdmin.js
@@ -1,6 +1,6 @@
 import admin from "firebase-admin";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY_JSON!);
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY_JSON);
 
 if (!admin.apps.length) {
   admin.initializeApp({
