77398bc5cf1e0de57b640af93c7fefbdf9cd60ad
14XLA99 <lars.aretz0@gmail.com>
2026-07-03 11:08:38 +0200

    Add 9 new offers handling in price storage

--- public/main.js
+++ public/main.js
@@ -651,18 +651,26 @@ nextBtn.onclick = async () => {
 
   try {
     // Preise flach in ein Objekt überführen: { "Produktname": Preis|null, ... }
-    const neuePreise = {};
-    produktPaare.forEach(({ p1, p2 }) => {
-      neuePreise[p1.name] = (typeof p1.preisErfasst === "number") ? p1.preisErfasst : (p1.preisErfasst ?? null);
-      neuePreise[p2.name] = (typeof p2.preisErfasst === "number") ? p2.preisErfasst : (p2.preisErfasst ?? null);
-    });
-  const groessenOverrides = preisDaten[currentSupermarkt]?.groessen || {};
-    await speicherePreisInFirestore(
-      currentSupermarkt,
-      neuePreise,
-      finaleBildUrl,
-      groessenOverrides   
-    );
+   const neuePreise = {};
+const neueAngebote = {};
+
+produktPaare.forEach(({ p1, p2 }) => {
+  neuePreise[p1.name] = (typeof p1.preisErfasst === "number") ? p1.preisErfasst : (p1.preisErfasst ?? null);
+  neuePreise[p2.name] = (typeof p2.preisErfasst === "number") ? p2.preisErfasst : (p2.preisErfasst ?? null);
+
+  neueAngebote[p1.name] = p1.angebot === true;
+  neueAngebote[p2.name] = p2.angebot === true;
+});
+
+const groessenOverrides = preisDaten[currentSupermarkt]?.groessen || {};
+
+await speicherePreisInFirestore(
+  currentSupermarkt,
+  neuePreise,
+  finaleBildUrl,
+  groessenOverrides,
+  neueAngebote
+);
 
     zuletztHochgeladenesBildURL = finaleBildUrl;
     if (!preisDaten[currentSupermarkt]) preisDaten[currentSupermarkt] = { preise: {}, bild: null };
